26a9804191c6626a4be3c26c20949fb401fdfc6b
caio myra <caiomyrapereira@hotmail.com>
2020-11-03 14:34:08 -0300

    Create Repository.

---
+++ src/main/java/com/generation/blog_pessoal/repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package com.generation.blog_pessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.generation.blog_pessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+}
