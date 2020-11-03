08ed292efca4f489fdb1b438afc707c0c8f33b62
caio myra <caiomyrapereira@hotmail.com>
2020-11-03 15:43:15 -0300

    Add uma método de trazer uma única postagem por 'ID'.

--- src/main/java/com/generation/blog_pessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blog_pessoal/controller/PostagemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +22,19 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository repository;
 
-	// Find all
+	// FindAll
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAllPostagens() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	
+	//FindById
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetByIdPostagem(@PathVariable  long id){
+		return repository.findById(id)
+				.map(response -> ResponseEntity.ok(response) )
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
