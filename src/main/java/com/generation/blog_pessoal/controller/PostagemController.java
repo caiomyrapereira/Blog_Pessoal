f4f50f5f79dff3fe437a087cbad3120b90f55fa4
caio myra <caiomyrapereira@hotmail.com>
2020-11-04 13:38:44 -0300

    Add método Post e Put.

--- src/main/java/com/generation/blog_pessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blog_pessoal/controller/PostagemController.java
@@ -3,10 +3,14 @@ package com.generation.blog_pessoal.controller;
 //labs
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,6 +55,19 @@ public class PostagemController {
 			return ResponseEntity.ok( repository.findAllByTituloContainingIgnoreCase(titulo) );
 		else
 			return ResponseEntity.notFound().build();
+	}
+	
+	
+	//Método posto
+	@PostMapping
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	//Método Put
+	@PutMapping
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 
 }
